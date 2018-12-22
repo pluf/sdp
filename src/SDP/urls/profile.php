6c8007944d679cee39cfefa3abae2d582690c6f1
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2018-12-23 01:49:49 +0330

    Edit structure of SDP_Profile. Add API for SDP_Profile.

---
+++ src/SDP/urls/profile.php
@@ -0,0 +1,65 @@
+<?php
+return array(
+    // ************************************************** SDP Profile
+    array( // Create / Update
+        'regex' => '#^/accounts/(?P<userId>\d+)/profiles$#',
+        'model' => 'SDP_Views_Profile',
+        'method' => 'update',
+        'http-method' => array(
+            'PUT',
+            'POST'
+        ),
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        )
+    ),
+    array( // Update
+        'regex' => '#^/accounts/(?P<userId>\d+)/profiles/(?P<profileId>\d+)$#',
+        'model' => 'SDP_Views_Profile',
+        'method' => 'update',
+        'http-method' => 'POST',
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        )
+    ),
+    array( // Read (list)
+        'regex' => '#^/accounts/(?P<parentId>\d+)/profiles$#',
+        'model' => 'Pluf_Views',
+        'method' => 'findManyToOne',
+        'http-method' => 'GET',
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        ),
+        'params' => array(
+            'parent' => 'User_Account',
+            'parentKey' => 'account_id',
+            'model' => 'SDP_Profile'
+        )
+    ),
+    array( // Read
+        'regex' => '#^/accounts/(?P<parentId>\d+)/profiles/(?P<modelId>\d+)$#',
+        'model' => 'Pluf_Views',
+        'method' => 'getManyToOne',
+        'http-method' => 'GET',
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        ),
+        'params' => array(
+            'parent' => 'User_Account',
+            'parentKey' => 'account_id',
+            'model' => 'SDP_Profile'
+        )
+    ),
+    array( // Delete
+        'regex' => '#^/accounts/(?P<userId>\d+)/profiles/(?P<profileId>\d+)$#',
+        'model' => 'SDP_Views_Profile',
+        'method' => 'delete',
+        'http-method' => 'DELETE',
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        )
+    )
+);
+
+
+

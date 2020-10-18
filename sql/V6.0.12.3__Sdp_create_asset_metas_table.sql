ff1f81f7de5343de87e2e8432d9ba899a5c82b36
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-10-18 12:51:03 +0330

    adding new entity asset-meta

---
+++ sql/V6.0.12.3__Sdp_create_asset_metas_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `sdp_asset_metas` (
+  `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(256) NOT NULL DEFAULT '',
+  `value` longtext,
+  `asset_id` mediumint(9) unsigned DEFAULT 0,
+  `tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key_unique_idx` (`tenant`, `asset_id`,`key`),
+  KEY `asset_id_foreignkey_idx` (`asset_id`),
+  KEY `tenant_foreignkey_idx` (`tenant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/SDP/AssetMeta.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 
+ */
+class SDP_AssetMeta extends Pluf_Model
+{
+
+    /**
+     * Initial content meta data
+     * 
+     * @see Pluf_Model::init()
+     */
+    function init()
+    {
+        $this->_a['table'] = 'sdp_asset_metas';
+        $this->_a['cols'] = array(
+            // Identifier
+            'id' => array(
+                'type' => 'Sequence',
+                'is_null' => false,
+                'editable' => false
+            ),
+            // Fields
+            'key' => array(
+                'type' => 'Varchar',
+                'is_null' => false,
+                'size' => 256,
+                'unique' => true,
+                'editable' => true
+            ),
+            'value' => array(
+                'type' => 'Text',
+                'is_null' => true,
+                'default' => '',
+                'editable' => true
+            ),
+            // Foreign keys
+            'asset_id' => array(
+                'type' => 'Foreignkey',
+                'model' => 'SDP_Asset',
+                'name' => 'asset',
+                'graphql_name' => 'asset',
+                'relate_name' => 'metas',
+                'is_null' => true,
+                'editable' => true
+            ),
+        );
+    }
+}
